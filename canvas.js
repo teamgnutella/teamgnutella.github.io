222713cf9d1025a5c40a80c82ef308fec1e5bab6
Nathan Craddock <nzcraddock@gmail.com>
2016-04-01 11:51:57 -0600

    Drawin
    making it better

--- canvas.js
+++ canvas.js
@@ -2,14 +2,17 @@ canvas = document.getElementById('canvas');
 context = canvas.getContext("2d");
 
 function drawLetter(letter, x, y) {
+	if(letter.length > 1) {
+		letter = letter[0]
+	}
+	
 	context.font = "12px Consolas";
 	context.textAlign = 'center'
-	context.fillText("0", x, y);
+	context.fillText(letter, x, y);
 }
 
 rows = 10
 cols = 10
-
 
 for(y = 0; y < canvas.height / rows; y++) {
 	for(x = 0; x < canvas.width / cols; x++) {

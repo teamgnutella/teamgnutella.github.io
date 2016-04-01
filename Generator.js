820e698c36b693c587c3a4fd72145e229e2ab281
DiddyManilla <diddymanilla@gmail.com>
2016-04-01 17:35:31 -0600

    Added some diagonals

--- Generator.js
+++ Generator.js
@@ -1,3 +1,5 @@
+//var sideLength = document.getElementById("width").value;
+var sideLength = 10;
 function Cell(str, bool) {
 	this.str = str;
 	this.isPartOfWord = bool;
@@ -12,9 +14,9 @@ function genSearch(wordList) {
 	//}
 	//Load "list" with random characters
 	var list = [];
-	for (var i = 0; i < 10; i++) {
+	for (var i = 0; i < sideLength; i++) {
 		list[i] = [];
-		for (var j = 0; j < 10; j++) {
+		for (var j = 0; j < sideLength; j++) {
 			list[i][j] = new Cell(alpha(), false);
 		}
 	}
@@ -33,11 +35,9 @@ function genSearch(wordList) {
 						
 						//Place word horizontally if there's room
 						if (isRoom(list, i, j, wordList[0], "h")) {
-							var r = 0;
 							var word = wordList.shift();
-							for (j; j < list[i].length && r < word.length; j++) {
+							for (var r = 0, j; j < list[i].length && r < word.length; j++, r++) {
 								list[i][j] = new Cell(word.charAt(r), true);
-								r++;
 							}
 						}
 						
@@ -45,11 +45,8 @@ function genSearch(wordList) {
 						//Place word vertically if there's room
 						if (isRoom(list, i, j, wordList[0], "v")) {
 							var word = wordList.shift();
-							var posInWord = 0;
-							var r = 0;
-							for (var k = i; k < list.length && r < word.length; k++) {
-								list[k][j] = new Cell(word.charAt(r), true);
-								r++;
+							for (var k = i, posInWord = 0; k < list.length && posInWord < word.length; k++, posInWord++) {
+								list[k][j] = new Cell(word.charAt(posInWord), true);
 							}
 						}
 					} else {
@@ -57,8 +54,14 @@ function genSearch(wordList) {
 						var direction = Math.random();
 						if (direction < 0.25) {
 							//Up and to the right
+							
 							if (isRoom(list, i, j, wordList[0], "ur")) {
-								
+								var word = wordList.shift();
+								for (var posInWord = 0, row = i, column = j;
+								row >= 0 && column < wordList[row].length && posInWord < word.length;
+								row--, column++) {
+									list[r][i] = new Cell(word.charAt(posInWord), true);
+								}
 							}
 						} else if (direction < 0.5) {
 							//Up and to the left
@@ -66,6 +69,14 @@ function genSearch(wordList) {
 							//Down and to the left
 						} else {
 							//Down and to the right
+							if (isRoom(list, i, j, wordList[0], "dr")) {
+								var word = wordList.shift();
+								for (var posInWord = 0, row = i, column = j;
+								posInWord < word.length, row < list.length, column < list[i].length;
+								posInWord++, row++, column++) {
+									list[row][column] = new Cell(word.charAt(posInWord), true);
+								}
+							}
 						}
 					}
 				}
@@ -96,10 +107,11 @@ function isRoom(list, ind1, ind2, wordToPlace, str) {
 		
 		if (wordToPlace.length > list[ind1].length - ind2) {
 			return false;
-		}
-		for (var j = ind2; j < list[ind1].length; j++) {
-			if (list[ind1][j].isPartOfWord) {
-				return false;
+		} else {
+			for (var j = ind2; j < list[ind1].length; j++) {
+				if (list[ind1][j].isPartOfWord) {
+					return false;
+				}
 			}
 		}
 		return true;
@@ -119,18 +131,47 @@ function isRoom(list, ind1, ind2, wordToPlace, str) {
 		
 	case "ur":
 		
-		if (wordToPlace.length > list[0].length - ind2 && wordToPlace.length > ind1 + 1) {
+		if (wordToPlace.length > ind1 + 1 || wordToPlace.length > list[ind1].length - ind2) {
 			return false;
 		} else {
-			
-			for (var i = ind1; i >= 0; i--) {
+			for (var i = ind1, j = ind2; i >= 0 && j < list[i].length; i--, j++) {
+				if (list[i][j].isPartOfWord) {
+					return false;
+				}
 			}
 		}
+		return true;
+		
 	case "ul":
 	
+		if (wordToPlace.length > ind1 + 1 || wordToPlace.length > ind2 + 1) {
+			return false;
+		} else {
+			for (var i = ind1, j = ind2; i >= 0, j >= 0; i--, j--) {
+				if (list[i][j].isPartOfWord) {
+					return false;
+				}
+			}
+		}
+		return true;
+		
 	case "dl":
 	
+		if (wordToPlace.length > list.length - ind1 || wordToPlace.length > ind2 + 1) {
+			
+		}
+			
 	case "dr":
+		if (wordToPlace.length > list.length - ind1 || wordToPlace.length > list[ind1].length - ind2) {
+			return false;
+		} else {
+			for (var i = ind1, j = ind2; i < list.length && j < list[i].length; i++, j++) {
+				if (list[i][j].isPartOfWord) {
+					return false;
+				}
+			}
+		}
+		return true;
 	}
 }
 //var filRea;

fb97952e8631e8e99a25e4824c9e4c91209b5083
TheKenCode <artist13kennyg13@gmail.com>
2016-04-01 10:18:11 -0700

    Java Length

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
 $(document).ready(function(){
 	var wordArray = [];
+	var setLength = 15;
 	$('#add').click(function(){
 		var word = $('#word').val();
 		$('#word-box').append(
 		'<li class="word">' + word + '</li>'
 		);
-		wordArray.push(word);
-		console.log(wordArray);
+		if(toBig(word)){
+			wordArray.push(word);
+			console.log(wordArray);
+		}
+		
 	});
+	function toBig(word){
+		var big = true;
+		if(word.length < setLength){
+			big = false;
+		}
+		return big;
+	}
 });

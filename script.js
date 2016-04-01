934e2a2d4827097f869ed8ccfc444d81107fe7f4
TheKenCode <artist13kennyg13@gmail.com>
2016-04-01 10:10:39 -0700

    Adding words to an array

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 $(document).ready(function(){
+	var wordArray = [];
 	$('#add').click(function(){
 		var word = $('#word').val();
 		$('#word-box').append(
 		'<li class="word">' + word + '</li>'
 		);
+		wordArray.push(word);
+		console.log(wordArray);
 	});
-	
 });

9d0c9d28291fc89935e877f84752971760e4850b
Nathan Craddock <nzcraddock@gmail.com>
2016-04-01 13:50:46 -0600

    Draw from letter
    :dancers:

--- canvas.js
+++ canvas.js
@@ -1,27 +1,60 @@
 canvas = document.getElementById('canvas');
 context = canvas.getContext("2d");
 
+// OPTIONS
+const drawGrid = true;
+
 function drawLetter(letter, x, y) {
 	if(letter.length > 1) {
-		letter = letter[0]
+		letter = letter[0];
 	}
-	
-	context.font = "12px Consolas";
-	context.textAlign = 'center'
+	context.fillStyle = 'black';
+	context.font = "12px Consolas red";
+	context.textAlign = 'center';
 	context.fillText(letter, x, y);
 }
 
-document.getElementById("add").addEventListener('click', getWords)
+document.getElementById("gen").addEventListener('click', generateWordSearch);
 
 function getWords() {
-	console.log(wordArray)
+	return wordArray;
 }
 
-rows = 10
-cols = 10
+rows = 10;
+cols = 10;
 
-for(y = 0; y < canvas.height / rows; y++) {
-	for(x = 0; x < canvas.width / cols; x++) {
-		drawLetter("0", (x * cols) + 5, (y * rows) + 9)
+function drawGridLines() {
+	context.save();
+	context.fillStyle = "#AAAAAA";
+	
+	for(y = 0; y < canvas.height / rows; y++) {
+		for(x = 0; x < canvas.width / cols; x++) {
+			
+		}
 	}
+	
+	
+	context.restore();
+}
+
+function drawLetters() {
+	numberX = 1;
+	numberY = 1;
+	
+	for(y = 0; y < rows; y++) {
+		for(x = 0; x < cols; x++) {
+			
+		}
+	}
+}
+
+function generateWordSearch() {
+	// Clear the canvas
+	context.clearRect(0, 0, canvas.width, canvas.height);
+	
+	if(drawGrid) {
+		drawGridLines();
+	}
+	
+	drawLetters();
 }
